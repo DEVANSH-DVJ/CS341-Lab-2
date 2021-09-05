cc10403014b0b57a8a6a2c10becf2c1b7af81970
devansh-dvj <devansh.dvj@gmail.com>
2021-09-05 18:58:15 +0530

    Q1: C - Finished C code

--- code/C/q1.c
+++ code/C/q1.c
@@ -1,5 +1,47 @@
 #include <stdio.h>
 
+int comb(int n, int r) {
+  if (r == 0)
+    return 1;
+
+  if (n == r)
+    return 1;
+
+  int res1 = comb(n - 1, r - 1);
+  int res2 = comb(n - 1, r);
+
+  return res1 + res2;
+}
+
 int main() {
+start:
+  printf("Enter n: ");
+
+  int n;
+  scanf("%i", &n);
+
+  printf("Enter r: ");
+
+  int r;
+  scanf("%i", &r);
+
+  int res = comb(n, r);
+  printf("%i", n);
+  printf("C");
+  printf("%i", r);
+  printf(": ");
+  printf("%i", res);
+  printf("\n");
+
+  printf("Wish to continue?: ");
+
+  char c;
+  scanf("%s", &c);
+
+  printf("\n");
+
+  if (c == 'Y')
+    goto start;
+
   return 0;
 }
