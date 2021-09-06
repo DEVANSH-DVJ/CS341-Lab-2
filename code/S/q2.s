9ae3a8a66741a943696629bd88b6c82781f2bb30
devansh-dvj <devansh.dvj@gmail.com>
2021-09-06 10:49:26 +0530

    Q2: Updated comments and temp variables

--- code/S/q2.s
+++ code/S/q2.s
@@ -31,17 +31,19 @@ newline:
 # $v1 -> y
 
 inv:
+  # if (a == 0) goto L1;
   beq $a0, $zero, L1
 
+  # x1, y1 = comb(b % a, a);
   addi $sp, $sp, -12
   sw $ra, 0($sp)
   sw $a0, 4($sp)
   sw $a1, 8($sp)
 
-  move $t0, $a0
+  move $t0, $a0 # t0 -> a
   divu $a1, $a0
-  mfhi $a0
-  move $a1, $t0
+  mfhi $a0 # a0 -> b % a
+  move $a1, $t0 # a1 -> a
   jal inv
   move $s0, $v0
   move $s1, $v1
@@ -51,18 +53,22 @@ inv:
   lw $a1, 8($sp)
   addi $sp, $sp, 12
 
+  # x = y1 - (b / a) * x1;
   div $a1, $a0
-  mflo $t0
-  mul $t1, $t0, $s0
-  sub $v0, $s1, $t1
+  mflo $t0 # t0 -> (b / a)
+  mul $t0, $t0, $s0 # t0 -> (b / a) * x1
+  sub $v0, $s1, $t0 # v0 -> y1 - (b / a) * x1
 
-  move $v1, $s0
+  # y = x1;
+  move $v1, $s0 # v1 -> x1
 
+  # return x, y;
   jr $ra
 
 L1:
-  addi $v0, $zero, 0
-  addi $v1, $zero, 1
+  # return 0, 1;
+  addi $v0, $zero, 0 # v0 -> 0
+  addi $v1, $zero, 1 # v1 -> 1
   jr $ra
 
 
@@ -111,10 +117,10 @@ main:
 
   # res = (x % m + m) % m;
   div $t0, $s1
-  mfhi $t1
-  add $t1, $t1, $s1
-  div $t1, $s1
-  mfhi $s2
+  mfhi $t2 # t2 -> x % m
+  add $t2, $t2, $s1 # t2 -> x % m + m
+  div $t2, $s1
+  mfhi $s2 # s2 -> (x % m + m) % m
 
   # printf("%i", a);
   li $v0, 1
