bfd799e4eac0659b4b78c96869b97cccb97338d9
devansh-dvj <devansh.dvj@gmail.com>
2021-09-06 07:16:51 +0530

    Added base case for factorial

--- PS/Resources/fact.s
+++ PS/Resources/fact.s
@@ -8,3 +8,21 @@ result:
 
 newline:
   .asciiz "\n"
+
+.text
+
+factorial:
+  # adjust stack pointer to store return address and argument
+  addi $sp, $sp, -8
+  sw $ra, 4($sp)
+  sw $a0, 0($sp)
+
+  bne $a0, 0, else # if x != 1 goto else;
+
+  # base case
+  addi $v0, $zero, 1 # return 1
+  j fact_return
+
+fact_return:
+  addi $sp, $sp, 8
+  jr $ra
