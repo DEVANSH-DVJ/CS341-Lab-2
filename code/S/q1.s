e5e500d8d735ef5282abab40f9ea2ab37a3e5e05
devansh-dvj <devansh.dvj@gmail.com>
2021-09-06 07:30:37 +0530

    Q1: Defined base case of comb

--- code/S/q1.s
+++ code/S/q1.s
@@ -18,3 +18,19 @@ colon:
 newline:
   .asciiz "\n"
 
+.text
+
+# $a0 -> n
+# $a1 -> r
+# $s0 -> res1
+# $s1 -> res2
+# $v0 -> output
+
+.comb:
+  beq $a0, $a1, L1
+  beq $a0, $zero, L1
+
+L1:
+  addi $v0, $zero, 1
+  jr $ra
+
