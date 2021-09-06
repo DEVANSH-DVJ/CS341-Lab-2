14ecca61da5e88e9022f24f0220c9972ab621520
devansh-dvj <devansh.dvj@gmail.com>
2021-09-06 07:17:01 +0530

    Added else case for factorial

--- PS/Resources/fact.s
+++ PS/Resources/fact.s
@@ -23,6 +23,18 @@ factorial:
   addi $v0, $zero, 1 # return 1
   j fact_return
 
+else:
+  addi $a0, $a0, -1
+  jal factorial # factorial(x-1)
+
+  # Reload stack variables
+  lw $ra, 4($sp)
+  lw $a0, 0($sp)
+
+  # when we get here, we already have v0 = (x-1)!
+  mul $v0, $a0, $v0 # return x * (x-1)!
+  j fact_return
+
 fact_return:
   addi $sp, $sp, 8
   jr $ra
