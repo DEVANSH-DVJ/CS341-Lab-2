b9c125cf424026db93074cf42665453822740879
devansh-dvj <devansh.dvj@gmail.com>
2021-09-06 10:32:47 +0530

    Q2: Added data

---
+++ code/S/q2.s
@@ -0,0 +1,23 @@
+.data
+
+enter_a:
+  .asciiz "Enter a: "
+
+enter_m:
+  .asciiz "Enter m: "
+
+prompt_continue:
+  .asciiz "Wish to continue?: "
+
+print_ast:
+  .asciiz "*"
+
+print_mod:
+  .asciiz " = 1 (mod "
+
+end_mod:
+  .asciiz ")\n"
+
+newline:
+  .asciiz "\n"
+
