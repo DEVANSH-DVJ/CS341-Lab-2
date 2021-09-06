70f358b18fef6f1959e358142208bcfdcaf274a0
devansh-dvj <devansh.dvj@gmail.com>
2021-09-06 07:29:54 +0530

    Q1: Added data

---
+++ code/S/q1.s
@@ -0,0 +1,20 @@
+.data
+
+enter_n:
+  .asciiz "Enter n: "
+
+enter_r:
+  .asciiz "Enter r: "
+
+continue:
+  .asciiz "Wish to continue?: "
+
+printC:
+  .asciiz "C"
+
+colon:
+  .ascii ": "
+
+newline:
+  .asciiz "\n"
+
